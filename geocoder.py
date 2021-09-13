3caacdd8486d86595ac1b1576157a0fb9351a2be
Parag <paragdurafe@outlook.com>
2021-09-14 01:09:26 +0530

    funtions added

--- geocoder.py
+++ geocoder.py
@@ -1,9 +1,20 @@
-from flask import Flask,render_template
+import pandas
+from flask import Flask,render_template,send_file,request
+from geopy.geocoders import Nominatim
+
 app=Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/sucess',methods=['POST'])
+def sucess():
+    return render_template('home.html')
+
+@app.route('/download')
+def download():
+    return send_file()
 
 if __name__=='__main__':
     app.run(debug=True)
