d4c449693a54df8780ade09e58319e378efe1a99
Parag <paragdurafe@outlook.com>
2021-09-15 21:50:22 +0530

    Coordinates Added

--- geocoder.py
+++ geocoder.py
@@ -1,4 +1,5 @@
-import pandas
+import datetime as dt
+import pandas as pd
 from flask import Flask,render_template,send_file,request
 from geopy.geocoders import Nominatim
 
@@ -8,8 +9,21 @@ app=Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/sucess',methods=['POST'])
-def sucess():
+@app.route('/success',methods=['POST'])
+def success():
+    global name_of_file
+    if request.method=="POST":
+        csv_file=request.files['file']
+
+        try:
+            data=pd.read_csv(csv_file)
+            coord=Nominatim()
+            data['Coordinates']=data['Address'].apply(coord.geocode)
+            data['Latitude']=data['Coordinates'].apply(lambda lat:lat.latitude if lat!=None else None)
+            data['Longitude']=data['Coordinates'].apply(lambda lon:lon.latitude if lon!=None else None)
+
+        except:
+            pass
     return render_template('home.html')
 
 @app.route('/download')
