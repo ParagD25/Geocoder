b19b7eff8337b23bd9d973fb15b4188537085399
Parag <paragdurafe@outlook.com>
2021-09-16 23:02:15 +0530

    Connecting Files

--- geocoder.py
+++ geocoder.py
@@ -13,22 +13,25 @@ def home():
 def success():
     global name_of_file
     if request.method=="POST":
-        csv_file=request.files['file']
+        file=request.files['file']
 
         try:
-            data=pd.read_csv(csv_file)
+            data=pd.read_csv(file)
             coord=Nominatim()
             data['Coordinates']=data['Address'].apply(coord.geocode)
             data['Latitude']=data['Coordinates'].apply(lambda lat:lat.latitude if lat!=None else None)
             data['Longitude']=data['Coordinates'].apply(lambda lon:lon.latitude if lon!=None else None)
+            data=data.drop('Coordinates',1)
+            name_of_file=dt.datetime.now().strftime("uploads/%m_%d_%M_%s"+".csv")
+            data.to_csv(name_of_file,index=None)
+            return render_template('home.html', text=data.to_html(), btn='download.html')
 
         except:
-            pass
-    return render_template('home.html')
+            return render_template('home.html',text='Please Enter Address Column in your csv file.')
 
-@app.route('/download')
+@app.route('/download/')
 def download():
-    return send_file()
+    return send_file(name_of_file,attachment_filename='Coordinates.csv',as_attachment=True)
 
 if __name__=='__main__':
     app.run(debug=True)
